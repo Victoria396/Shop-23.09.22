54e056a7f39f3f6ad4c599e1247bc9b8afda2900
Victoria396 <95283353+Victoria396@users.noreply.github.com>
2022-09-23 10:56:43 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000, img: 'img/notebook.webp'},
+    {id: 2, title: 'Mouse', price: 20, img: 'img/mouse.webp'},
+    {id: 3, title: 'Keyboard', price: 200, img: 'img/keyboard.jpg'},
+    {id: 4, title: 'Gamepad', price: 50, img: 'img/gamepad.jpg'},
+];
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = (item) => {
+    return `<div class="product-item">
+                <div>
+                    <img src="${item.img}" alt="gamepad">
+                </div>
+                <h3>${item.title}</h3>
+                <p>${item.price}</p>
+                <button class="buy-btn">Buy</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList.join("");;
+};
+
+renderPage(products);
